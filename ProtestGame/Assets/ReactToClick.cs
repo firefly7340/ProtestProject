4409f748ae69d03a9057658da9c6884186eae0b8
Stephen Wood <Stephen Wood>
2016-11-25 17:26:11 +0000

    added some basic object interaction

---
+++ ProtestGame/Assets/ReactToClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactToClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown("mouse 0"))
+        {
+            //print("hello world");
+        }
+	}
+
+    void OnMouseDown()
+    {
+        print("Box Clicked!");
+    }
+}
